02cc0bf026be813a0d7fb9bd0b12b52eaeb5390f
Josep Crego <josep.crego@systrangroup.com>
2024-10-14 12:19:49 +0200

    output

--- whisper/infer.py
+++ whisper/infer.py
@@ -12,32 +12,40 @@ class infer():
         logging.info(f'DONE')
 
     def __call__(self, audio, fdo, split_stereo=False, transcribe={}):
-        fdo.write(f"model={self.model_name}" + "\n")
-        fdo.write(f"audio={audio}" + "\n")
-        fdo.write(f"split_stereo={split_stereo}" + "\n")
-        fdo.write(f"load={self.load}" + "\n")
-        fdo.write(f"transcribe={transcribe}" + "\n")
+        self.fdo = fdo
+        self.out = []
+        self.add(f"model={self.model_name}")
+        self.add(f"audio={audio}")
+        self.add(f"split_stereo={split_stereo}")
+        self.add(f"load={self.load}")
+        self.add(f"transcribe={transcribe}")
 
         logging.info(f'READING {audio} ...')
         tic = time.time()
         audio = decode_audio(audio, split_stereo=split_stereo)
-        fdo.write(f"reading_time={time.time()-tic:.2f} sec" + "\n")
+        self.add(f"reading_time={time.time()-tic:.2f} sec")
         if not isinstance(audio, tuple):
             audio = [audio]
-        fdo.write(f"audio_duration={len(audio[0])/16000} sec" + "\n")
-        fdo.write(f"channels={len(audio)}" + "\n")
+        self.add(f"audio_duration={len(audio[0])/16000} sec")
+        self.add(f"channels={len(audio)}")
     
         for channel in range(len(audio)):
             logging.info(f'TRANSCRIBING channel {channel+1} ...')
             tic = time.time()
             segments, info = self.model.transcribe(audio[channel], **transcribe)
-            fdo.write(f"ch={channel+1} language_predicted={info.language} probability={info.language_probability}" + "\n")
+            self.add(f"ch={channel+1} language_predicted={info.language} probability={info.language_probability}")
             for segment in segments:
                 if transcribe.get('word_timestamps') is True:
                     for word in segment.words:
-                        fdo.write(f"ch={channel+1} start={word.start:.3f} end={word.end:.3f} txt={word.word}" + "\n")
+                        self.add(f"ch={channel+1} start={word.start:.3f} end={word.end:.3f} txt={word.word}")
                 else:
-                    fdo.write(f"ch={channel+1} start={segment.start:.3f} end={segment.end:.3f} txt={segment.text}" + "\n")
-            fdo.write(f"ch={channel+1} inference_time={time.time()-tic:.2f} sec" + "\n")
+                    self.add(f"ch={channel+1} start={segment.start:.3f} end={segment.end:.3f} txt={segment.text}")
+            self.add(f"ch={channel+1} inference_time={time.time()-tic:.2f} sec")
         logging.info(f'DONE')
+        return self.out
 
+    def add(self, line):
+        if self.fdo is not None:
+            self.fdo.write(line + '\n')
+        else:
+            self.out.append(line)
