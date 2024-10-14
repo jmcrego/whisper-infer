58f71f772392fb97eca83accd6b21146a26f21a2
Josep Crego <josep.crego@systrangroup.com>
2024-10-14 19:48:20 +0200

    light

--- scripts/infer.py
+++ scripts/infer.py
@@ -11,29 +11,31 @@ class infer():
         self.model = WhisperModel(model_name, **load)
         logging.info(f'DONE')
 
-    def __iter__(self, audio, split_stereo=False, transcribe={}):
-        yield f"model={self.model_name}"
-        yield f"audio={audio}"
-        yield f"split_stereo={split_stereo}"
-        yield f"load={self.load}"
-        yield f"transcribe={transcribe}"
-
+    def __call__(self, audio, split_stereo=False, transcribe={}):
+        self.audio_name = audio
+        self.split_stereo = split_stereo
+        self.transcribe = transcribe
         logging.info(f'READING {audio} ...')
         tic = time.time()
-        audio = decode_audio(audio, split_stereo=split_stereo)
-        yield f"reading_time={time.time()-tic:.2f} sec"
-        if not isinstance(audio, tuple):
-            audio = [audio]
-        yield f"audio_duration={len(audio[0])/16000} sec"
-        yield f"channels={len(audio)}"
-    
-        for channel in range(len(audio)):
+        self.audio = decode_audio(audio, split_stereo=split_stereo)
+        if not isinstance(self.audio, tuple):
+            audio = [self.audio]
+
+    def __iter__(self):
+        yield f"model={self.model_name}"
+        yield f"audio={self.audio_name}"
+        yield f"split_stereo={self.split_stereo}"
+        yield f"load={self.load}"
+        yield f"transcribe={self.transcribe}"
+        yield f"audio_duration={len(self.audio[0])/16000} sec"
+        yield f"channels={len(self.audio)}"
+        for channel in range(len(self.audio)):
             logging.info(f'TRANSCRIBING channel {channel+1} ...')
             tic = time.time()
-            segments, info = self.model.transcribe(audio[channel], **transcribe)
+            segments, info = self.model.transcribe(self.audio[channel], **self.transcribe)
             yield f"ch={channel+1} language_predicted={info.language} probability={info.language_probability}"
             for segment in segments:
-                if transcribe.get('word_timestamps') is True:
+                if self.transcribe.get('word_timestamps') is True:
                     for word in segment.words:
                         yield f"ch={channel+1} start={word.start:.3f} end={word.end:.3f} txt={word.word}"
                 else:
