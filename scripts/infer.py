a3b3ac3be5faa8c10df5593f8a4cc39652857e2a
Josep Crego <josep.crego@systrangroup.com>
2024-10-14 17:48:08 +0200

    yield

--- scripts/infer.py
+++ scripts/infer.py
@@ -11,40 +11,34 @@ class infer():
         self.model = WhisperModel(model_name, **load)
         logging.info(f'DONE')
 
-    def __call__(self, audio, fdo, split_stereo=False, transcribe={}):
-        self.fdo = fdo
-        self.out = []
-        self.add(f"model={self.model_name}")
-        self.add(f"audio={audio}")
-        self.add(f"split_stereo={split_stereo}")
-        self.add(f"load={self.load}")
-        self.add(f"transcribe={transcribe}")
+    def __iter__(self, audio, split_stereo=False, transcribe={}):
+        yield f"model={self.model_name}"
+        yield f"audio={audio}"
+        yield f"split_stereo={split_stereo}"
+        yield f"load={self.load}"
+        yield f"transcribe={transcribe}"
 
         logging.info(f'READING {audio} ...')
         tic = time.time()
         audio = decode_audio(audio, split_stereo=split_stereo)
-        self.add(f"reading_time={time.time()-tic:.2f} sec")
+        yield f"reading_time={time.time()-tic:.2f} sec"
         if not isinstance(audio, tuple):
             audio = [audio]
-        self.add(f"audio_duration={len(audio[0])/16000} sec")
-        self.add(f"channels={len(audio)}")
+        yield f"audio_duration={len(audio[0])/16000} sec"
+        yield f"channels={len(audio)}"
     
         for channel in range(len(audio)):
             logging.info(f'TRANSCRIBING channel {channel+1} ...')
             tic = time.time()
             segments, info = self.model.transcribe(audio[channel], **transcribe)
-            self.add(f"ch={channel+1} language_predicted={info.language} probability={info.language_probability}")
+            yield f"ch={channel+1} language_predicted={info.language} probability={info.language_probability}"
             for segment in segments:
                 if transcribe.get('word_timestamps') is True:
                     for word in segment.words:
-                        self.add(f"ch={channel+1} start={word.start:.3f} end={word.end:.3f} txt={word.word}")
+                        yield f"ch={channel+1} start={word.start:.3f} end={word.end:.3f} txt={word.word}"
                 else:
-                    self.add(f"ch={channel+1} start={segment.start:.3f} end={segment.end:.3f} txt={segment.text}")
-            self.add(f"ch={channel+1} inference_time={time.time()-tic:.2f} sec")
+                    yield f"ch={channel+1} start={segment.start:.3f} end={segment.end:.3f} txt={segment.text}"
+            yield f"ch={channel+1} inference_time={time.time()-tic:.2f} sec"
         logging.info(f'DONE')
-        return self.out
 
-    def add(self, line):
-        if self.fdo is not None:
-            self.fdo.write(line + '\n')
-        self.out.append(line)
+
--- whisper-infer.py
+++ whisper-infer.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Script to run inference on Whisper models. Ex: python whisper-infer.py small --audio example.wav --load '{\"device\": \"cuda\", \"compute_type\": \"int8\"}' --transcribe '{\"language\": \"en\", \"beam_size\": 5, \"word_timestamps\": true}'")
     parser.add_argument('model', type=str, help='Path or name of the Whisper model. (https://github.com/SYSTRAN/faster-whisper/blob/d57c5b40b06e59ec44240d93485a95799548af50/faster_whisper/utils.py#L12)')
     parser.add_argument('--audio', type=str, required=True, help='Audio file to transcribe.')    
-    parser.add_argument('--output', type=str, default=None, required=False, help='File where transcriptions are output (use stdout to print on STDOUT).')
+    parser.add_argument('--output', type=str, default=None, required=False, help='File where where to print transcriptions (or print on STDOUT).')
     parser.add_argument('--split_stereo', action='store_true', help='Split channels of (stereo) audio files')
     parser.add_argument('--load', type=str, default=None, required=False, help="JSON dictionary to control model loading. (https://github.com/SYSTRAN/faster-whisper/blob/d57c5b40b06e59ec44240d93485a95799548af50/faster_whisper/transcribe.py#L583)")
     parser.add_argument('--transcribe', type=str, default=None, required=False, help="JSON dictionary to control transcription. (https://github.com/SYSTRAN/faster-whisper/blob/d57c5b40b06e59ec44240d93485a95799548af50/faster_whisper/transcribe.py#L705)")
@@ -18,15 +18,12 @@ if __name__ == '__main__':
     logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=getattr(logging, 'WARNING' if not args.debug else 'INFO'), filename=None)
     
     load = json.loads(args.load) if args.load is not None else {}
-    w = infer(args.model, load=load)
-
-    if args.output is None:
-        fdo = None
-    elif args.output.lower() == 'stdout':
-        fdo = sys.stdout
-    else:
-        fdo = open(args.output, 'w')
     transcribe = json.loads(args.transcribe) if args.transcribe is not None else {}
-    res = w(args.audio, fdo=fdo, split_stereo=args.split_stereo, transcribe=transcribe)
+    fdo = sys.stdout if args.output is None else open(args.output, 'w')
 
-    
+    w = infer(args.model, load=load)
+    for l in w(args.audio, fdo=fdo, split_stereo=args.split_stereo, transcribe=transcribe):
+        fdo.write(l + '\n')
+
+    if args.output is not None:
+        fdo.close()
