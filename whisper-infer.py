6be213acfc468f91f70906c442402c430a542392
Josep Crego <josep.crego@systrangroup.com>
2024-10-15 11:05:19 +0200

    light

--- whisper-infer.py
+++ whisper-infer.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Script to run inference on Whisper models. Ex: python whisper-infer.py small --audio example.wav --load '{\"device\": \"cuda\", \"compute_type\": \"int8\"}' --transcribe '{\"language\": \"en\", \"beam_size\": 5, \"word_timestamps\": true}'")
     parser.add_argument('model', type=str, help='Path or name of the Whisper model. (https://github.com/SYSTRAN/faster-whisper/blob/d57c5b40b06e59ec44240d93485a95799548af50/faster_whisper/utils.py#L12)')
     parser.add_argument('--audio', type=str, required=True, help='Audio file to transcribe.')    
-    parser.add_argument('--output', type=str, default=None, required=False, help='File where where to print transcriptions (or print on STDOUT).')
+    parser.add_argument('--output', type=str, default=None, required=False, help='File where to print transcriptions (or print on STDOUT).')
     parser.add_argument('--split_stereo', action='store_true', help='Split channels of (stereo) audio files')
     parser.add_argument('--load', type=str, default=None, required=False, help="JSON dictionary to control model loading. (https://github.com/SYSTRAN/faster-whisper/blob/d57c5b40b06e59ec44240d93485a95799548af50/faster_whisper/transcribe.py#L583)")
     parser.add_argument('--transcribe', type=str, default=None, required=False, help="JSON dictionary to control transcription. (https://github.com/SYSTRAN/faster-whisper/blob/d57c5b40b06e59ec44240d93485a95799548af50/faster_whisper/transcribe.py#L705)")
