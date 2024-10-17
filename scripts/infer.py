73af7ca4cb56ac0bbd903643b33749a05769bbc1
Josep Crego <josep.crego@systrangroup.com>
2024-10-17 15:30:56 +0200

    refactor

--- scripts/infer.py
+++ scripts/infer.py
@@ -1,46 +1,46 @@
 import time
 import logging
+import numpy as np
+from scipy.io import wavfile
 from faster_whisper import WhisperModel, decode_audio
 
 class infer():
 
-    def __init__(self, model_name, load={}):
-        self.model_name = model_name
-        self.load = load
-        logging.info(f'LOADING {model_name} ...')
-        self.model = WhisperModel(model_name, **load)
-        logging.info(f'DONE')
-
-    def __call__(self, audio, split_stereo=False, transcribe={}):
-        self.audio_name = audio
-        self.split_stereo = split_stereo
-        self.transcribe = transcribe
-        logging.info(f'READING {audio} ...')
-        tic = time.time()
-        self.audio = decode_audio(audio, split_stereo=split_stereo)
+    def __init__(self, model, audio, split_stereo=False, load={}):
+        # model is either a Systran model (medium, small, ...) or the path to a local checkpoint
+        # audio is either a string containing the path to an audio file or a numpy.ndarray resulting from decode_audio
+        logging.info(f'model={model}')
+        logging.info(f"load={load}")
+        self.model = WhisperModel(model, **load)
+        self.audio = decode_audio(audio, split_stereo=split_stereo) if isinstance(audio, str) else audio
         if not isinstance(self.audio, tuple):
             self.audio = [self.audio]
+        logging.info(f"channels={len(self.audio)}")
+        logging.info(f"duration={len(self.audio[0])/16000} sec")
 
-    def __iter__(self):
-        yield f"model={self.model_name}"
-        yield f"audio={self.audio_name}"
-        yield f"split_stereo={self.split_stereo}"
-        yield f"load={self.load}"
-        yield f"transcribe={self.transcribe}"
-        yield f"audio_duration={len(self.audio[0])/16000} sec"
-        yield f"channels={len(self.audio)}"
-        for channel in range(len(self.audio)):
-            logging.info(f'TRANSCRIBING channel {channel+1} ...')
-            tic = time.time()
-            segments, info = self.model.transcribe(self.audio[channel], **self.transcribe)
-            yield f"ch={channel+1} language_predicted={info.language} probability={info.language_probability}"
+    def __call__(self, channel=None, start=None, end=None, transcribe={}, save=None):
+        logging.info(f"channel={channel}")
+        logging.info(f"start={start}")
+        logging.info(f"end={end}")
+        logging.info(f"transcribe={transcribe}")
+        channel = 0 if channel is None else channel-1
+        start = 0 if start is None else int(start*16000)
+        end = len(self.audio[0]) if end is None else int(end*16000)
+        res = []
+        for ch in range(len(self.audio)):
+            if channel != ch:
+                continue
+            segments, info = self.model.transcribe(self.audio[ch][start:end], **transcribe)
             for segment in segments:
-                if self.transcribe.get('word_timestamps') is True:
+                if transcribe.get('word_timestamps') is True:
                     for word in segment.words:
-                        yield f"ch={channel+1} start={word.start:.3f} end={word.end:.3f} txt={word.word}"
+                        res.append({'ch': ch+1, 'start': word.start, 'end': word.end, 'txt': word.word})
                 else:
-                    yield f"ch={channel+1} start={segment.start:.3f} end={segment.end:.3f} txt={segment.text}"
-            yield f"ch={channel+1} inference_time={time.time()-tic:.2f} sec"
-        logging.info(f'DONE')
+                    res.append({'ch': ch+1, 'start': segment.start, 'end': segment.end, 'txt': segment.text})
+        if save is not None:
+            print(f'save into {save}')
+            wavfile.write(save, 16000, np.int16(self.audio[channel][start:end] * 32767))
+        return res
 
+        
 
--- whisper-infer-pakita.py
+++ whisper-infer-pakita.py
@@ -1,6 +1,7 @@
 import os
 import sys
-import json
+from tqdm import tqdm
+from json import loads
 import logging
 import argparse
 from scripts.infer import infer
@@ -9,30 +10,60 @@ from faster_whisper import WhisperModel, decode_audio
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="Script to run inference on Whisper models. Ex: python whisper-infer.py small --audio example.wav --load '{\"device\": \"cuda\", \"compute_type\": \"int8\"}' --transcribe '{\"language\": \"en\", \"beam_size\": 5, \"word_timestamps\": true}'")
+    parser = argparse.ArgumentParser(description="Script to run inference on Whisper models. Ex: python whisper-infer.py small --audio example.wav --load '{\"device\": \"default\", \"compute_type\": \"default\"}' --transcribe '{\"language\": \"fr\", \"vad_filter\": false}'")
     parser.add_argument('model', type=str, help='Path or name of the Whisper model. (https://github.com/SYSTRAN/faster-whisper/blob/d57c5b40b06e59ec44240d93485a95799548af50/faster_whisper/utils.py#L12)')
-    parser.add_argument('--audio', type=str, required=True, help='Audio file to transcribe.')    
+    parser.add_argument('--ids', type=str, required=True, help='File with list of audio files.')    
     parser.add_argument('--reference', type=str, default=None, help='Reference transcription with segmentation timestamps.')    
+    parser.add_argument('--output', type=str, default=None, help='Output path for [output].(ref,hyp) transcriptions.')    
     parser.add_argument('--load', type=str, default=None, required=False, help="JSON dictionary to control model loading. (https://github.com/SYSTRAN/faster-whisper/blob/d57c5b40b06e59ec44240d93485a95799548af50/faster_whisper/transcribe.py#L583)")
     parser.add_argument('--transcribe', type=str, default=None, required=False, help="JSON dictionary to control transcription. (https://github.com/SYSTRAN/faster-whisper/blob/d57c5b40b06e59ec44240d93485a95799548af50/faster_whisper/transcribe.py#L705)")
+    parser.add_argument('--debug', action='store_true', help='Verbose mode')
     args = parser.parse_args()
-    logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=getattr(logging, 'WARNING' if args.silent else 'INFO'), filename=None)
+    logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=getattr(logging, 'INFO' if args.debug else 'WARNING'), filename=None)
 
-    load = json.loads(args.load) if args.load is not None else {}
-    transcribe = json.loads(args.transcribe) if args.transcribe is not None else {}
+    sampling_rate = 16000
+    prefix_wav = 'corpus/pakita/fre/cts/data/audio/'
+    prefix_trs = 'corpus/pakita/fre/cts/data/trans/deta/stm/'
 
-    w = infer(args.model, load=load)
+    with open(args.ids, 'r') as fdi:
+        names = [l.strip() for l in fdi]
+    references = [prefix_trs+name+'.stm' for name in names]
+    audios = [prefix_wav+name+'.wav' for name in names]
 
-    refs = []
-    hyps = []
-    audio = decode_audio(args.audio, split_stereo=True)
-    for ch in range(1, 3):
-        ref = ref2list(args.reference, channel=ch)
-        for e in ref:
-            print(f"{e['beg']}, {e['end']}, {e['txt']}")
-            w(audio[e['beg']:e['end']], split_stereo=False, transcribe=transcribe)
-            refs.append(e['txt'])
-            hyps.append(''.join([l for l in w]))
-                        
-    s = jiwer_wrap(uppercase=True, no_punct=True, no_hesit=False, no_noise=True, split_apos=True)
-    s(refs, hyps)
+    load = loads(args.load) if args.load is not None else {}
+    transcribe = loads(args.transcribe) if args.transcribe is not None else {}
+    
+    s = jiwer_wrap(uppercase=True, no_punct=True, no_hesit=True, no_noise=True, split_apos=True)
+    w = infer(args.model, load=load, transcribe=transcribe)
+
+    if args.output is not None:
+        fdh = open(args.output+'.hyp', 'w')
+        fdr = open(args.output+'.ref', 'w')
+        fde = open(args.output+'.err', 'w')
+
+    refs, hyps = [], []
+    for i in range(len(audios)):
+        audio = decode_audio(audios[i], split_stereo=True)
+        assert (len(audio) == 2)
+
+        for ch in range(len(audio)):
+            ref = ref2list(references[i], channel=ch+1)
+            for k in tqdm(range(len(ref)), desc=f'channel:{ch}'):
+                beg = int(float(ref[k]['beg'])*sampling_rate)
+                end = int(float(ref[k]['end'])*sampling_rate)
+                refs.append(ref[k]['txt'])
+                hyps.append(w.transcription(audio[ch][beg:end]))
+                wer = s(refs[-1], hyps[-1])
+                if args.output is not None:
+                    fdh.write(f"{hyps[-1]}\n")
+                    fdh.flush()
+                    fdr.write(f"{refs[-1]}\n")
+                    fdr.flush()
+                    fde.write(f"{names[i]}:{ch}:{ref[k]['beg']}-{ref[k]['end']} {wer}\n")
+                    fde.flush()
+            print(s(refs, hyps))
+
+    if args.output is not None:
+        fdh.close()
+        fdr.close()
+        fde.close()
